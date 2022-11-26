35b7e9ddf89cfac0431724ead47eda852a655730
lyu-bel <lyu-bel@yandex.ru>
2022-11-26 15:45:00 +0700

    edit comments

--- Project0/guess_the_number_binary_search.py
+++ Project0/guess_the_number_binary_search.py
@@ -2,12 +2,13 @@ def binary_search():
     import numpy as np
     n = 100
     number = np.random.randint(1, n+1) # загадываем число
-    count=0  #число попыток 
-    low = 0  #наменьшее значение
-    high = n+1 #наибольшее значение
+    count = 0                          
+    low = 0                            
+    high = n+1                         
     while low<=high:
         count+=1
         mid = (low+high)//2
+        
         if mid<number:
             low=mid
         if mid>number:
@@ -15,8 +16,9 @@ def binary_search():
         if mid==number:
             return count
 
+
 # Function call 
-sum=0
+sum_runs = 0
 for i in range(1000):
-    sum+=binary_search()
-print(f'Программа угадает ваше число в среднем за {sum/1000} попыток')
+    sum_runs += binary_search()
+print(f'Программа угадает ваше число в среднем за {sum_runs/1000} попыток')
