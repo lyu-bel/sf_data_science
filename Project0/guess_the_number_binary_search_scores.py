08f7c559e7fc40addcb4f03fc611eb40d161440b
lyu-bel <lyu-bel@yandex.ru>
2022-11-27 00:47:10 +0700

    add func scores

--- Project0/guess_the_number_binary_search_scores.py
+++ Project0/guess_the_number_binary_search_scores.py
@@ -1,33 +1,32 @@
-# Вызо функции бинарного поиска с рекурсией
-def binary_search_rec(number, count, low, high):
-    mid = (low+high)//2
-    count += 1  
+def binary_search(number,count, low, high): #Бинарный поиск с циклом 1000 прохождений
     
-    if mid == number:
-      return count
-    
-    if mid < number:
-      low = mid
-      return binary_search_rec(number,count, low, high)  
-    
-    if mid > number:
-      high = mid
-      return binary_search_rec(number, count, low, high)
+    while low <= high:
+        count += 1
+        mid = (low+high) //2
+        
+        if mid < number:
+            low = mid
+            
+        if mid > number:
+            high = mid
+            
+        if mid == number:
+            return count
 
 
 # Вызов функции нахождения количества попыток в среднем из 1000 подходов угадывает наш алгоритм
-def score_game(binary_search_rec) -> int:
+def score_game(binary_search) -> int:
     n=100    # верхняя граница значений
     count=0  # счетчик кол-во попыток начальное значение
     low=0    # нижняя граница начальное значение
     high=n+1   # верхняя граница начальное значение
   
     count_ls = [] # список для сохранения количества попыток
-    np.random.seed(15) # фиксируем сид для воспроизводимости
+    np.random.seed(11) # фиксируем сид для воспроизводимости
     random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
 
     for number in random_array:
-        count_ls.append(binary_search_rec(number,count, low, high))
+        count_ls.append(binary_search(number,count, low, high))
 
     score = int(np.mean(count_ls)) # находим среднее количество попыток
     return(score)
@@ -35,5 +34,5 @@ def score_game(binary_search_rec) -> int:
 # Вызов функции
 import numpy as np
 
-score_game=score_game(binary_search_rec)
+score_game=score_game(binary_search)
 print(f'Ваш алгоритм угадывает число в среднем за: {score_game} попыток')
