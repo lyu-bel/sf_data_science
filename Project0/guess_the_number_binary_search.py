0feee93903badf9c94c3eaea593e7fecbeb91276
lyu-bel <lyu-bel@yandex.ru>
2022-11-26 16:11:19 +0700

    mod project0

--- Project0/guess_the_number_binary_search.py
+++ Project0/guess_the_number_binary_search.py
@@ -1,24 +1,30 @@
-def binary_search():
+def binary_search(): #Бинарный поиск с циклом 1000 прохождений
     import numpy as np
     n = 100
     number = np.random.randint(1, n+1) # загадываем число
-    count = 0                          
-    low = 0                            
-    high = n+1                         
+    count = 0    #счетчик попыток угадывания                      
+    low = 0      #нижняя граница                      
+    high = n+1   #верхняя граница                     
+    
     while low<=high:
         count+=1
         mid = (low+high)//2
         
-        if mid<number:
+        if mid < number:
             low=mid
-        if mid>number:
-            high=mid
-        if mid==number:
+            
+        if mid > number:
+            high = mid
+            
+        if mid == number:
             return count
 
 
-# Function call 
+# Вызываем функцию
+
 sum_runs = 0
+
 for i in range(1000):
     sum_runs += binary_search()
+    
 print(f'Программа угадает ваше число в среднем за {sum_runs/1000} попыток')
--- Project0/guess_the_number_binary_search_rec.py
+++ Project0/guess_the_number_binary_search_rec.py
@@ -1,11 +1,14 @@
 def binary_search_rec(number, count, low, high): #Бинарный поиск с рекурсией с циклом 1000 прохождений
     mid = (low+high)//2
-    count+=1  #число попыток 
+    count += 1  
+    
     if mid == number:
       return count
+    
     if mid < number:
       low = mid
       return binary_search_rec(number,count, low, high)  
+    
     if mid > number:
       high = mid
       return binary_search_rec(number, count, low, high)
@@ -13,14 +16,14 @@ def binary_search_rec(number, count, low, high): #Бинарный поиск с
 
 # Вызов функции
 import numpy as np
-n = 100
-sum = 0
+n = 100  #верхняя граница диапазона угадывания
+sum = 0 
 
 for i in range (1000):
   number = np.random.randint(1, n+1) # загадываем число
-  count = 0
-  low = 0  #наменьшее значение
-  high = n+1 #наибольшее значение
-  sum += binary_search_rec(number, count, low,high)
+  count = 0   #счетчик попыток угадывания
+  low = 0  #нижняя граница
+  high = n+1 #верхняя граница
+  sum += binary_search_rec(number, count, low, high)
 
 print(f'Программа угадает ваше число в среднем за {sum/1000} попыток')
